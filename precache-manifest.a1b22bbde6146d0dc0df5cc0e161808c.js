624674d0e5ca74d461fabe68d037d7ff1e6f6f2e
Gisele <giselesousar@gmail.com>
2020-05-08 23:12:05 -0300

    updates

---
+++ precache-manifest.a1b22bbde6146d0dc0df5cc0e161808c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e4e79aff9b29263b53575c75bdddfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a52b3db0ecad515e50",
+    "url": "/static/css/main.98362c05.chunk.css"
+  },
+  {
+    "revision": "e51444039fe554e12e92",
+    "url": "/static/js/2.d8cc1982.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d8cc1982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a52b3db0ecad515e50",
+    "url": "/static/js/main.6be30004.chunk.js"
+  },
+  {
+    "revision": "9653abae4b6fbcede268",
+    "url": "/static/js/runtime-main.63cb50d0.js"
+  }
+]);
