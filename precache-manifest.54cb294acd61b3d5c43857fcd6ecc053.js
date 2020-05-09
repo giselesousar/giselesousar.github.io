5c35044880ae51ad8a3a022a31726b91cfa44b0e
Gisele <giselesousar@gmail.com>
2020-05-09 09:28:20 -0300

    fix routes

---
+++ precache-manifest.54cb294acd61b3d5c43857fcd6ecc053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89b7dddf9daa6dfa775e0208aae6246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39f2f7871f6bab006d61",
+    "url": "/static/css/main.3b02e862.chunk.css"
+  },
+  {
+    "revision": "2f292bbe8eb5665c7669",
+    "url": "/static/js/2.709275c3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.709275c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f2f7871f6bab006d61",
+    "url": "/static/js/main.1e1a0c65.chunk.js"
+  },
+  {
+    "revision": "9653abae4b6fbcede268",
+    "url": "/static/js/runtime-main.63cb50d0.js"
+  }
+]);
