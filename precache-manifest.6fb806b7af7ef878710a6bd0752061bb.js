d3f75eb287667fc4ddcfe97013edd5da53a59d7b
Gisele <giselesousar@gmail.com>
2020-05-09 22:20:40 -0300

    fix navbar

---
+++ precache-manifest.6fb806b7af7ef878710a6bd0752061bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109c2581fb85a6bb18da0e5a1ffe53ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d76f5dd7b11091aaee",
+    "url": "/static/css/main.15cf961b.chunk.css"
+  },
+  {
+    "revision": "e584736f30968235fbc1",
+    "url": "/static/js/2.ce379b8d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ce379b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d76f5dd7b11091aaee",
+    "url": "/static/js/main.97f5a6b8.chunk.js"
+  },
+  {
+    "revision": "9653abae4b6fbcede268",
+    "url": "/static/js/runtime-main.63cb50d0.js"
+  }
+]);
